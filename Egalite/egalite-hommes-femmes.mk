74719d85deb0ff0769d1b81f5bcd3d26485783bf
Blue Prawn <monnier.florent@gmail.com>
2014-04-11 13:21:35 +0159

    switch from wget to curl

--- Egalite/egalite-hommes-femmes.mk
+++ Egalite/egalite-hommes-femmes.mk
@@ -1,5 +1,5 @@
 CAT = cat
-WGET = curl -s
+CURL = curl -s
 
 OD_WEB = https://www.data.gouv.fr
 OD_DIR = /storage/f/2013-11-28T16%3A14%3A39.248Z/
@@ -8,7 +8,7 @@ OD_FILE = observatoire-egalite-mcc-rapport-fevrier-2013-1.odt
 .PHONY: dl
 dl: $(OD_FILE)
 $(OD_FILE):
-	$(WGET) $(OD_WEB)$(OD_DIR)$(OD_FILE) > $@
+	$(CURL) $(OD_WEB)$(OD_DIR)$(OD_FILE) > $@
 
 clean:
 	$(RM) $(OD_FILE)
